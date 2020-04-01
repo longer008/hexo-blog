79ad5bcb9fbe21a53d466d262a0d9d271d37102e
longer <937723369@qq.com>
2020-04-01 16:48:34 +0800

    update

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 git add --all 
 git commit  -m 'update'
-git push -u origin master
+git push  origin master
 
 # git subtree split -P public -b master
 
@@ -14,9 +14,22 @@ git push -u origin master
 
 
 echo -e "\ngit commit successful!"
+
+hexo clean
+hexo deploy
+
+cp -r public/* ../longer008.github.io/
+cd  ../longer008.github.io/
+git add --all 
+git commit  -m 'update'
+git push  origin master
+
 exit 0
 
 # 把所有 `public` 目录下的相关提交整理为一个新的分支 master
 # git subtree split -P public -b master
 
 # --allow-unrelated-histories
+
+# git删除远程分支
+# git push origin --delete [branch_name]
