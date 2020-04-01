51c5d6927b2b3a42068a19cae28b5a55ee19c843
longer <937723369@qq.com>
2020-04-01 17:38:53 +0800

    update

--- themes/amazing/layout/common/footer.jsx
+++ themes/amazing/layout/common/footer.jsx
@@ -23,23 +23,6 @@ class Footer extends Component {
                         <a class="footer-logo is-block mb-2" href={siteUrl}>
                             {logo && logo.text ? logo.text : <img src={logoUrl} alt={siteTitle} height="28" />}
                         </a>
-                        <p class="size-small">
-                            <span dangerouslySetInnerHTML={{ __html: `&copy; ${siteYear} ${author || siteTitle}` }}></span>
-                            &nbsp;&nbsp;Powered by <a href="https://hexo.io/" target="_blank">Hexo</a> & <a
-                                href="https://github.com/ppoffice/hexo-theme-icarus" target="_blank">Icarus</a>,Modify by <a href="https://github.com/removeif" target="_blank">removeif</a>&nbsp;
-                            <br />
-                            &copy; 版权说明：[本网站所有内容均收集于互联网或自己创作,<br />&nbsp;&nbsp;&nbsp;&nbsp;方便于网友与自己学习交流，如有侵权，请<a href={url_for('/message')} target="_blank">留言</a>，立即处理]
-                            <br />
-                            <script type="text/javascript" src={my_cdn('/js/statistics.js')}></script>
-                            <span id="statistic-times">网站运行时间统计加载中...</span>
-                            <br />
-                            {showVisitorCounter ? <div class="size-small"><span id="busuanzi_container_site_uv">
-                                ❤️感谢<strong>&nbsp;<span id="busuanzi_value_site_uv">99+</span>&nbsp;</strong>
-                            </span>小伙伴的
-                                <strong>&nbsp;<span id="busuanzi_value_site_pv">99+</span>&nbsp;</strong>次光临，查看💐<a href="https://github.com/removeif/hexo-theme-amazing" target="_blank">主题源码</a>！❤️</div> : null}
-                        </p>
-                    </div>
-                    <div class="level-end">
                         {Object.keys(links).length ? <div class="field has-addons">
                             {Object.keys(links).map(name => {
                                 const link = links[name];
@@ -50,7 +33,34 @@ class Footer extends Component {
                                 </p>;
                             })}
                         </div> : null}
+                        <p class="size-small">
+                            <span dangerouslySetInnerHTML={{ __html: `&copy; ${siteYear} ${author || siteTitle}` }}></span>
+                            &nbsp;&nbsp;Powered by <a href="https://hexo.io/" target="_blank">Hexo</a> & <a
+                                href="https://github.com/ppoffice/hexo-theme-icarus" target="_blank">Icarus</a>,Modify by <a href="https://github.com/removeif" target="_blank">Walter</a>&nbsp;
+                            <br />
+                            &copy; 版权说明：[本网站所有内容均收集于互联网或自己创作,<br />&nbsp;&nbsp;&nbsp;&nbsp;方便于网友与自己学习交流，如有侵权，请<a href={url_for('/message')} target="_blank">留言</a>，立即处理]
+                            <br />
+                            <script type="text/javascript" src={my_cdn('/js/statistics.js')}></script>
+                            <span id="statistic-times">网站运行时间统计加载中...</span>
+                            <br />
+                            {showVisitorCounter ? <div class="size-small"><span id="busuanzi_container_site_uv">
+                                ❤️感谢<strong>&nbsp;<span id="busuanzi_value_site_uv">99+</span>&nbsp;</strong>
+                            </span>小伙伴的
+                                <strong>&nbsp;<span id="busuanzi_value_site_pv">99+</span>&nbsp;</strong>次光临！❤️</div> : null}
+                        </p>
                     </div>
+                    {/* <div class="level-end">
+                        {Object.keys(links).length ? <div class="field has-addons">
+                            {Object.keys(links).map(name => {
+                                const link = links[name];
+                                return <p class="control">
+                                    <a class={`button is-transparent ${link.icon ? 'is-large' : ''}`} target="_blank" rel="noopener" title={name} href={link.url}>
+                                        {link.icon ? <i class={link.icon}></i> : name}
+                                    </a>
+                                </p>;
+                            })}
+                        </div> : null}
+                    </div> */}
                 </div>
             </div>
         </footer>;
