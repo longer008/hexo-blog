0e3cedf420724a4cd8cf89d3bef703ca454f84b8
longer <937723369@qq.com>
2020-04-01 15:28:40 +0800

    update _config.yaml

--- commit.sh
+++ commit.sh
@@ -1,5 +1,8 @@
 git add --all
 git commit  -m 'update'
-git push 
+git push origin dev
 echo -e "\ngit commit successful!"
 exit 0
+
+# 把所有 `public` 目录下的相关提交整理为一个新的分支 master
+# git subtree split -P public -b master
