4aba4e19198ee13ed731321f636fdebd7d363769
longer <937723369@qq.com>
2020-04-02 11:47:06 +0800

    update

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-// author by walter
-// https://longer008.github.io/
 function createtime() {
     var n = new Date("04/01/2020 00:00:00");
     now.setTime(now.getTime() + 250),
@@ -14,13 +12,12 @@ function createtime() {
         seconds = (now - n) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum,
         snum = Math.round(seconds),
     1 == String(snum).length && (snum = "0" + snum),
-        document.getElementById("statistic-times").innerHTML = "❤️本站自<span><strong> 2020.04.01</strong><span> 已运行 <strong>" + dnum + "</strong> 天 <strong>" + hnum + "</strong> 小时 <strong>" + mnum + "</strong> 分 <strong>" + snum + "</strong> 秒！❤️";
+        console.log("❤️本站自<span><strong> 2020.04.01</strong><span> 已运行 <strong>" + dnum + "</strong> 天 <strong>" + hnum + "</strong> 小时 <strong>" + mnum + "</strong> 分 <strong>" + snum + "</strong> 秒！❤️");
 }
 
 var now = new Date;
 try {
     setInterval(createtime(), 250);
 } catch (error) {
-    console.log("")
-
-// setInterval("createtime()", 250);
+    console.log("time error")
+}
